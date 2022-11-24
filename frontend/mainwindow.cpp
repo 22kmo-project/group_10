12e420422a9aa6d466882f7624a8cce0caed1655
miikkaniska <78212251+miikkaniska@users.noreply.github.com>
2022-11-24 19:59:33 +0200

    Eka QstackWidgetillä toteutettu frontend.

--- frontend/mainwindow.cpp
+++ frontend/mainwindow.cpp
@@ -6,10 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_loginButton_clicked()
+{
+    if (ui->idEdit->text() == ""){
+        popupBox.setText("Tyhjä ID-kenttä");
+        popupBox.exec();
+    }
+    else if (ui->pinEdit->text() == ""){
+        popupBox.setText("Tyhjä PIN-kenttä");
+        popupBox.exec();
+    }
+    else {
+        ui->stackedWidget->setCurrentIndex(1);
+    }
+}
+
+
+void MainWindow::on_logoutButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
